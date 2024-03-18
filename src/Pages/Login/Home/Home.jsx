96071e188ac9fb2a9ac0a3d9e6f4f676d3a31dc3
Shaunak <ssnagvenkar@gmail.com>
2024-03-18 11:39:46 +0530

    Changes to home page

--- src/Pages/Login/Home/Home.jsx
+++ src/Pages/Login/Home/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="bg-white">
+        <div className=''></div>
+      </div>
+    </>
   )
 }
--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -5,7 +5,7 @@ export default function Login() {
     <>
 
       <div className="bg-yellow-300 flex flex-col h-screen align-middle justify-center items-center rounded-3xl">
-        <div className="w-4/12 bg-white">
+        <div className="w-4/12 bg-white rounded-2xl border border-10 border-black border-opacity-20">
           <form type='submit' className='p-10'>
             <input placeholder='Username' className='rounded-lg border border-sm border-black border-opacity-30 h-8 w-4/5'></input>
           </form>
